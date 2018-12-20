35591894ade6627b7eebf85da1e99163bebfe1bd
Amariah Moseley <amoseley@unityprep.org>
2018-12-20 23:03:17 +0000

    hi

--- js/script.js
+++ js/script.js
@@ -8,7 +8,36 @@ $(document).ready(function() {
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
+        var q4Result = $("#question4").val();
         var totalScore;
     });
 
 });
+
+function question1(q1Result) {
+    var points; 
+    points= q1Result.length();
+    return  points
+}
+function question2(q2Result) {
+    var points; 
+    points= q2Result
+    return  points
+}
+function question3(q3Result) {
+    var points; 
+    points= q3Result
+    return  points
+}
+function question4(q4Result) {
+    var points; 
+    points= q4Result
+    return  points
+}
+
+
+
+
+
+
+
